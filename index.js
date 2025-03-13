93ee1e21e879dc78bbc126911c773300ef811b99
panidaputh <panida.phx@gmail.com>
2025-03-13 15:11:16 +0700

    changes time cooldown for 5hr

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ app.post("/webhook", async (req, res) => {
       const userData = snapshot.val() || {};
       const lastFallbackTime = userData.lastFallbackTime || 0;
       const currentTime = Date.now();
-      const COOLDOWN_PERIOD = 28800000;
+      const COOLDOWN_PERIOD = 18000000;
 
       if (currentTime - lastFallbackTime >= COOLDOWN_PERIOD) {
         await userRef.update({
